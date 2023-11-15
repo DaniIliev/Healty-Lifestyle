62d76217d2f4d80498bd1e070ffa8ad280d8119c
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2023-11-15 20:26:36 +0200

    create Header Component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+export default function Header(){
+
+
+    return(
+        <header className="site-header">
+            <div className="wrapp-nav">
+                    <img src="" alt="logo" className="logo"/>
+                    <nav className="site-navigation">
+                        <ul>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='recipes'>RECIPES</Link></li>
+                            <li><Link to='/user/login'>LOGIN</Link></li>
+                            <li><Link to='/user/register'>SIGN UP</Link></li>
+                        </ul>
+                    </nav>
+            </div>
+
+        </header>
+    )
+}
