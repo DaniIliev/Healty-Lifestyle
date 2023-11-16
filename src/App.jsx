e9cef0e1adcb49b5fd816f8e4b651d107f4d7215
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2023-11-16 10:59:28 +0200

    update home page css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import Header from './components/header/Header'
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +10,12 @@ function App() {
   return (
 <>
       <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+
+      <Footer/>
 </>
   )
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,90 @@
+import {Link} from 'react-router-dom'
+
+export default function Home(){
+
+
+    return(
+        <>
+        <section className="home">
+            <div className="homeWrapp">
+                <div className="homeImg">
+                    <img src="./images/home.png" alt="" />
+                </div>
+                <div className="text">
+                    <h1>Welcome to Healty Lifestyle</h1>
+                    <h3>Here you will discover how to eat deliciously and healthily</h3>
+                    <p>If you have a hidden culinary talent, the recipes on this site will make sure you develop it</p>
+
+                    <div className='button'>
+                        <p>
+                            <Link to='/recipes'>Begint Your Colinary Journey</Link>
+                            <img src="/images/icons/computer-mouse.svg" alt="mouse" />
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>  
+            <div className="whyChoise">
+                <h3>Why should you choose Healthy Lifestyle</h3>
+                <img src="/images/love.png" alt="love" />
+            </div>
+
+            <div className="advantagesWrapp">
+                <div className="advantage">
+                    <h3>01</h3>
+                    <div className="textContent">
+                    <h4>
+                        A wealth of healthy recipes
+                    </h4>
+                    <p>
+                    Anyone registered on this site can share their experience in healthy eating by adding a recipe</p>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <h3>02</h3>
+                    <div className="textContent">
+                    <h4>
+                        A wealth of healthy recipes
+                    </h4>
+                    <p>Anyone registered on this site can share their experience in healthy eating by adding a recipe</p>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <h3>03</h3>
+                    <div className="textContent">
+                    <h4>
+                        Preparation of a personal healty food plan
+                    </h4>
+                    <p>By entering your weight, height, activity level and age, the calorie calculator will calculate your daily calorie intake based on your goal.</p>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <h3>04</h3>
+                    <div className="textContent">
+                    <h4>
+                        Constant Updates
+                    </h4>
+                    <p>New recipes are added daily.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="join">
+                    <div className="media">
+                         <img src="/images/finger.png" alt="finger" />
+                    </div>  
+                    <div className="textJoin">
+                        <h3>Hey you, join the Healthy Lifestyle team today</h3>
+                        <h4>Start your healthy lifestyle</h4>
+                        <h4>Unleash Your Inner Chef!</h4>
+                        <p><Link>Create an account</Link></p>
+                    </div>
+            </div>
+        </section>
+        
+        </>
+    )
+}
